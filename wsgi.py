0ba461bfc9a604455fda247427079dd399bb4796
dfear <david@pickle -it.co.uk>
2023-07-17 23:00:52 +0100

    Create the WSGI Entry Point - wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -6,4 +6,8 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
+<<<<<<< HEAD
     app.run()
+=======
+    app.run()
+>>>>>>> e58836b (Create the WSGI Entry Point - wsgi.py)
